593432ff3159aa64d8dc9f39b88426c88d92116e
KhalifRich <105115632+KhalifRich@users.noreply.github.com>
2022-11-18 11:31:17 +0300

    Add files via upload

---
+++ Architect3DGalleryWeb/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Architect3DGalleryWeb.DataAccess.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Architect3DGalleryWeb.Areas.Identity.IdentityHostingStartup))]
+namespace Architect3DGalleryWeb.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
